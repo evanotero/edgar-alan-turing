663f7ff43bc6338e6565f3a03cdbfb58eadd51dd
James LeDoux <ledouxja@bc.edu>
2016-10-23 01:18:44 -0400

    fix issue with dictionaries and translation

--- scripts/generate_text.py
+++ scripts/generate_text.py
@@ -6,33 +6,42 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
-filename = "/Users/jamesledoux/Documents/edgar-alan-turing/shakespeare"
+
+filename = "/Users/jamesledoux/Documents/edgar-alan-turing/text_files/shakespeare"
 raw_text = open(filename).read()
 raw_text = raw_text.lower()
 badtext = '/:;][{}-=+)(*&^%-_$#@!~1234567890'
 textfile = raw_text.translate(None, badtext)
+
 # create mapping of unique chars to integers, and a reverse mapping
-chars = sorted(list(set(raw_text)))
+chars = sorted(list(set(textfile)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
 int_to_char = dict((i, c) for i, c in enumerate(chars))
+# print char_to_int
+# print int_to_char
+
 # summarize the loaded data
-n_chars = len(raw_text)
+n_chars = len(textfile)
 n_vocab = len(chars)
-print "Total Characters: ", n_chars
-print "Total Vocab: ", n_vocab
+# print "Total Characters: ", n_chars
+# print "Total Vocab: ", n_vocab
+
 # prepare the dataset of input to output pairs encoded as integers
 seq_length = 100
 dataX = []
 dataY = []
 for i in range(0, n_chars - seq_length, 1):
-	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
+    seq_in = textfile[i:i + seq_length]
+    seq_out = textfile[i + seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
+
 n_patterns = len(dataX)
-print "Total Patterns: ", n_patterns
+# print "Total Patterns: ", n_patterns
+
 # reshape X to be [samples, time steps, features]
 X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+
 # normalize
 X = X / float(n_vocab)
 # one hot encode the output variable
@@ -44,30 +53,33 @@ model.add(Dropout(0.2))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
+
 # load the network weights
-filename = "/Users/jamesledoux/Desktop/weights-improvement-07-1.9626.hdf5"
+#filename = "/Users/jamesledoux/Desktop/weights-improvement-13-1.9537.hdf5"  #dante
+filename = "/Users/jamesledoux/Desktop/weights-improvement-31-1.4242.hdf5"  #willie
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
-# pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-#a random seed taken from the text
-#print "Seed:"
-#print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
 
-#trying out a custom seed
-print "seed: "
-pattern = "                                                      i am hungry. what is for dinner? i hope it is "
-print pattern
+# pick a random seed
+start = numpy.random.randint(0, len(dataX) - 1)
+pattern = dataX[start]
+# a random seed taken from the text
+print "Seed:"
+print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
+
+# trying out a custom seed
+#pattern = "                                                             to be or not to be that is the question"
+#print pattern
 pattern = [char_to_int[c] for c in pattern]
-# generate characters
+
+#generate characters
 for i in range(100):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(n_vocab)
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = int_to_char[index]
+    seq_in = [int_to_char[value] for value in pattern]
+    sys.stdout.write(result)
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
